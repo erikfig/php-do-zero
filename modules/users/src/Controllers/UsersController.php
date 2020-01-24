c1a6ca23952e0459746bc8dc12c2df5c1a9c3097
Erik Figueiredo <erik.figueiredo@gmail.com>
2020-01-24 03:28:17 -0300

    correções quarto artigo

--- modules/users/src/Controllers/UsersController.php
+++ modules/users/src/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace ErikFig\Framework\Users\Controllers;
 use App\Controllers\AppController;
 use ErikFig\Framework\Users\Models\User;
 
+// aqui eu herdo os recursos na AppController
 class UsersController extends AppController
 {
     public function index()
--- modules/users/src/Register.php
+++ modules/users/src/Register.php
@@ -9,9 +9,11 @@ class Register
 {
     public static function handle(Environment $twig, Router $router)
     {
+        // Aqui registro a rota
         $loader = $twig->getLoader();
         $loader->addPath(__DIR__ . '/../templates');
 
+        // Aqui registro as rotas
         $router->get('/users', 'ErikFig\Framework\Users\Controllers\UsersController::index');
     }
 }
